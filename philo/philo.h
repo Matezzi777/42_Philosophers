b818e5f56ed3e1b8a99c405697006228f6f8cf95
Max Martinage <maxmart2@f6r12s1.clusters.42paris.fr>
2025-08-12 18:17:33 +0200

    Project's Settlement

---
+++ philo/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 18:05:54 by maxmart2          #+#    #+#             */
+/*   Updated: 2025/08/12 18:13:22 by maxmart2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdlib.h>	//malloc(), free()
+# include <string.h>	//memset()
+# include <stdio.h>		//stdio()
+# include <unistd.h>	//write(), usleep()
+# include <sys/time.h>	//gettimeofday()
+# include <pthread.h>	//pthread_create(), pthread_detach(), pthread_join()
+						//pthread_mutex_init(), pthread_mutex_destroy()
+						//pthread_mutex_lock(), pthread_mutex_unlock()
+
+#endif
